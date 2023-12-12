2162fc6bbc17899b60138f426fe50b5dfc6c35a5
Ben Woodard <benjaminwoodard93@gmail.com>
2023-12-11 20:05:57 -0500

    added CustomList and CustomListTest Class
    added classes for Test Driven Development and added testing for add method

--- src/com/coderscampus/benWoodardAssignment7/CustomArrayList.java
+++ src/com/coderscampus/benWoodardAssignment7/CustomArrayList.java
@@ -1,5 +1,40 @@
 package com.coderscampus.benWoodardAssignment7;
 
-public class CustomArrayList {
+import java.lang.reflect.Array;
+
+public class CustomArrayList<T> implements CustomList {
+
+	Object[] items = new Object[10];
+	private int size = 0;
+
+	@Override
+	public boolean add(Object item) {
+		items[size] = item;
+		size++;
+		return false;
+	}
+
+	@Override
+	public boolean add(int index, Object item) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object get(int index) throws IndexOutOfBoundsException {
+		return items[index];
+	}
+
+	@Override
+	public Object remove(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- test/com/coderscampus/benWoodardAssignment7/CustomArrayListTest.java
+++ test/com/coderscampus/benWoodardAssignment7/CustomArrayListTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 class CustomArrayListTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void should_add_one_item_to_list() {
+		@SuppressWarnings("unchecked")
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		sut.add(0);
+		
+		assertEquals(0, sut.get(0));
 	}
 
+	@Test
+	void should_add_11_items_to_list () {
+		@SuppressWarnings("unchecked")
+		CustomList<Integer> sut = new CustomArrayList<>();
+		for(int i = 0; i < 11; i++) {
+			sut.add(i);
+		}
+		
+		assertEquals(10, sut.get(11));
+	}
 }
